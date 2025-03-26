7d377ebee927a9e8f69c49290d14dd1b08d04d3c
Gabriella Freitas <107807653+GabriellaFsena@users.noreply.github.com>
2025-03-26 20:19:14 -0300

    atividade 2 e 3

---
+++ atividade-3/roupasSA.py
@@ -0,0 +1,6 @@
+nome = "GABRIELLA"
+mes = 'JANEIRO'
+valor = 500
+desconto = 10
+
+print(f"Olá, {nome}. Em {mes}, você realizou uma compra no valor de {valor} e ganhou um desconto de {desconto}% em sua próxima compra. O cupom é {nome}É10.")
