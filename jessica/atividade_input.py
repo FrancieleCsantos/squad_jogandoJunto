0a7269ad4404ee3596a2ea6d6d771e6c284e9ad7
Jessica Arf <jessicalealarf8@gmail.com>
2025-03-27 20:53:53 -0300

    feat: realização da primeira parte da atividade do dia 27/03

---
+++ jessica/atividade_input.py
@@ -0,0 +1,10 @@
+#Faça um programa que capture o nome do usuário, altura em metros, idade e imprima esses dados na tela.
+
+nome = input("Seja bem-vindo! Qual é o seu nome? ")
+altura = input(f"Olá, {nome}! Qual é sua altura (em metros) ? ")
+idade = input(f"Quantos anos você tem, {nome}? ")
+
+print(f"\n{nome}, você tem {idade} anos e mede {altura} metros. Legal, né?")
+
+
+
