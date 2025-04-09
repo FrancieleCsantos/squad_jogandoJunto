3c72cec1dfd43826a0ae77bfa4f2ab532c77b53a
Gabriella Freitas <107807653+GabriellaFsena@users.noreply.github.com>
2025-04-09 20:49:57 -0300

    Atividade PetShop

---
+++ gabriella/petshopt.py
@@ -0,0 +1,12 @@
+# case Idade do Pet e Lucro do PETSHOP
+nome_pet = input("Qual o nome do pet?")
+
+idade_humana = int(input("Qual é a idade do seu cachorro? "))
+
+idade_cachorro = idade_humana * 7
+
+
+print(f"Legal!{nome_pet} tem {idade_cachorro} anos humanos. ")
+
+
+
