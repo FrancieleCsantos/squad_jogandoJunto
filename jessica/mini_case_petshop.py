80e0d1e4316eb542aef6c3cd7356ebd27768d823
Jessica Arf <jessicalealarf8@gmail.com>
2025-04-09 21:25:00 -0300

    refactor: alteração das variáveis locais para globais

--- jessica/mini_case_petshop.py
+++ jessica/mini_case_petshop.py
@@ -10,26 +10,25 @@ print("2 - Porte médio")
 print("3 - Porte grande")
 porte_cachorro = input("Digite a opção: ")
 
+valor_banho = 0
+custo_banho = 0
+
 if porte_cachorro == "1":
     valor_banho = 50
     custo_banho = 5
-    lucro = valor_banho - custo_banho
-    calculo_lucro_meses = lucro * 12
-    print(f"Olá, {nome_cachorro} tem {calculo_idade} anos e nos últimos 12 meses o lucro com este animal foi de R${calculo_lucro_meses:.2f}.")
 elif porte_cachorro == "2":   
     valor_banho = 60
     custo_banho = 15
-    lucro = valor_banho - custo_banho
-    calculo_lucro_meses = lucro * 12
-    print(f"Olá, {nome_cachorro} tem {calculo_idade} anos e nos últimos 12 meses o lucro com este animal foi de R${calculo_lucro_meses:.2f}.")
 elif porte_cachorro == "3":   
     valor_banho = 75
     custo_banho = 20
-    lucro = valor_banho - custo_banho
-    calculo_lucro_meses = lucro * 12
-    print(f"Olá, {nome_cachorro} tem {calculo_idade} anos e nos últimos 12 meses o lucro com este animal foi de R${calculo_lucro_meses:.2f}.")
 else:
     print("Opção de porte selecionada desconhecida.")
+
+
+lucro = valor_banho - custo_banho
+calculo_lucro_meses = lucro * 12
+print(f"Olá, {nome_cachorro} tem {calculo_idade} anos e nos últimos 12 meses o lucro com este animal foi de R${calculo_lucro_meses:.2f}.")
 
 
 
